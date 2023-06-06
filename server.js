37d328e0bd8aeac2c4a405483d2f1ae0f3c86891
Nofi-nof <takenoko2402@gmail.com>
2023-06-06 19:39:45 +0900

    rootに移動

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fastify = require('fastify')({
 })
 
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  return 'hello world'
 })
 
 /**
@@ -15,7 +15,6 @@ const start = async () => {
     await fastify.listen({ port: 3000 })
   } catch (err) {
     fastify.log.error(err)
-    process.exit(1)
   }
 }
 start()
