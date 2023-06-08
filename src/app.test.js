0b0fb6793ae45eaa126e1072c9ad734db87c10e6
Nofi-nof <takenoko2402@gmail.com>
2023-06-08 23:45:39 +0900

    Hello Worldが返ってきているか確認する自動テストを書いてみた

---
+++ src/app.test.js
@@ -0,0 +1,16 @@
+import { afterAll, expect, test } from 'vitest'
+import { app } from './app.js'
+
+test('Hello Worldが返却されること', async () => {
+  const response = await app.inject({
+    method: 'GET',
+    url: '/',
+  })
+
+  expect(response.statusCode).toBe(200)
+  expect(response.payload).toBe('hello world')
+})
+
+afterAll(async () => {
+  await app.close()
+})
