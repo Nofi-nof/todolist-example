4a0c2c5505edf6f44c9ebe7a0e3ca0907578fb7b
Nofi-nof <takenoko2402@gmail.com>
2023-06-18 20:31:30 +0900

    TypeScript移行時のESlintの設定修正

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import { app } from "./app";
 /**
  * Run the server!
  */
-const start = async () => {
+const start = async (): Promise<void> => {
   try {
     await app.listen({ port: 3000 });
   } catch (err) {
     app.log.error(err);
   }
 };
-start();
+start().catch((err) => {
+  app.log.error(err);
+});
