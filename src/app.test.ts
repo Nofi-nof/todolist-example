5ec88e3ff803728ad76a5850b953daf8fdf23e07
Nofi-nof <takenoko2402@gmail.com>
2023-06-28 23:44:02 +0900

    テストの文を減らした

--- src/app.test.ts
+++ src/app.test.ts
@@ -9,8 +9,6 @@ test("Hello Worldが返却されること", async () => {
   });
 
   expect(response.statusCode).toBe(200);
-  expect(response.payload).toMatch(/hello world/);
-
   const $ = load(response.payload);
   expect($("h1").text().replace(/\n/g, "").trim()).toBe("hello world");
 });
